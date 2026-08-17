5786a1e145b5843ffc869fcead04e8e1e5f1f458
vaibhavpatilvaibhu-alt <vaibhavpatilvaibhu@gmail.com>
2026-08-17 15:56:38 +0530

    Add files via upload

---
+++ prg10/src/prg10/demo.java
@@ -0,0 +1,23 @@
+package prg10;
+
+public class demo {
+	int a=66;
+	static int cc =99;
+	void m1() {
+		int a=10;
+		System.out.println("aef"+a);
+	}
+	public demo() {
+		int b=20;
+		System.out.println("sjdfwrfv="+b);
+	}
+	public static void main(String[] args) {
+		demo test=new demo();
+		test.m1();
+		System.out.println("tyhank you"+test.a);
+		System.out.println(cc);
+		System.out.println(demo.cc);
+	}
+	
+
+}
